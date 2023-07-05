a7833301555a2bc04e2ab5a49397899188d65d28
Vivian Kho <122664885+svftbuns@users.noreply.github.com>
2023-07-06 01:48:59 +0800

    activation layer

--- activation.py
+++ activation.py
@@ -1,16 +1,4 @@
-# Define individual activation layers
-# ReLU layer
-class ReLU(Layer):
-    
-    def forward_prop(self, input):
-        self.input=input
-        return np.maximum(0,input)
-    
-    # Compute the gradient of the loss function wrt input of the activation layer
-    # output_gradient: Gradient of lost wrt output of the activation layer, passed from subsequent layer
-    def backward_prop(self, output_gradient,alpha):
-        return np.multiply(output_gradient, self.input>0)
-    
+# Define individual activation layers 
 # Sigmoid Layer
 class Sigmoid(Layer):
     
@@ -33,12 +21,9 @@ class Softmax(Layer):
         top = np.exp(shifted_input)
         self.output = top / np.sum(top)
         return self.output
-    
-    # When used in conjuction with cross-entropy loss, derivative of softmax is just the output gradient
-    def backward_prop(self,output_gradient,alpha):
-        return output_gradient
 
-        """n = np.size(self.output)
+    def backward_prop(self,output_gradient,alpha):
+        n = np.size(self.output)
         matrix=np.dot((np.identity(n) - self.output.T) * self.output, output_gradient)
-        return np.multiply(output_gradient,matrix)"""
+        return np.multiply(output_gradient,matrix)
         
